e51814c20fb422a76b6aa2023eafcd05bcfc56dd
sgrootveld <sgrootveld@gmail.com>
2019-03-26 22:33:38 +0100

    Creating template

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar is-info">
+    <div class="container">
+        <div class="navbar-brand">
+            <a class="navbar-item">
+                <h1>Oostpolder Online</h1>
+            </a>
+        </div>
+
+        <div id="navbarMenu" class="navbar-menu is-white">
+            <div class="navbar-end">
+
+                <span class="navbar-item is-white">
+                    <a class="is-white" href="#">
+                        <span class="is-white">Algemene informatie</span>
+                    </a>
+                </span>
+
+                <span class="navbar-item">
+                    <a class="is-white" href="#">
+                        <span>Activiteiten</span>
+                    </a>
+                </span>
+
+                <span class="navbar-item">
+                    <a class="is-white" href="#">
+                        <span>Nieuws</span>
+                    </a>
+                </span>
+
+                <span class="navbar-item">
+                    <a class="is-white" href="#">
+                        <span>Contact</span>
+                    </a>
+                </span>
+
+                <span class="navbar-item">
+                    <a class="is-white" href="#">
+                        <span>Bewoners</span>
+                    </a>
+                </span>
+            </div>
+        </div>
+    </div>
+</nav>
